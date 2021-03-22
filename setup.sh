367525f488478a079a2738d47acbb2adb473d4ce
sky <sky012877@gmail.com>
2021-03-22 10:56:09 -0700

    setup.sh update 2

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 echo Base Linux settings
+CONF_DIR=$(dirname $(realpath "&0"))/config
+INIT_VIM=$CONF_DIR/init.vim
 
 echo My aliases on ...
+ln -sf $CONF_DIR/my_aliases ~/my_aliases && echo ". ~/my_aliases" >> ~/.bashrc
+ls -la ~/my_aliases
+tail -3 ~/.bashrc
 
-ln -s $(dirname $(readlink -f $0))/my_aliases ~/my_aliases && echo ". ~/my_aliases" >> ~/.bashrc
+echo init.vim on ...
+mkdir -p ~/.config/nvim/
+ln -sf $INIT_VIM ~/.config/nvim/init.vim
+ls -la ~/.config/nvim/init.vim
+ln -sf $INIT_VIM ~/.vimrc
+ls -la ~/.vimrc 
