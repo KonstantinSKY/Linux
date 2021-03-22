7f4e77a87052066ca03fac026a6bfe6a8746310c
sky <sky012877@gmail.com>
2021-03-21 22:44:46 -0700

    NO MESSAGE

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ echo Base Linux settings
 
 echo My aliases on ...
 
-ln -s $(dirname $(readlink -f $0))/my_aliases ~/my_aliases && echo ". my_aliases" >> ~/.bashrc
+ln -s $(dirname $(readlink -f $0))/my_aliases ~/my_aliases && echo ". ~/my_aliases" >> ~/.bashrc
