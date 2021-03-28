45c9e31201a55201a1fdca09db239f1b1c155612
sky <sky012877@gmail.com>
2021-03-28 01:15:15 -0700

    modyfi 2 files

--- new.sh
+++ new.sh
@@ -2,14 +2,24 @@
 
 # Creating  new executing bash file...
 
-if [$1] 
+if [ $1 ] 
 then
     file_name=$1
 else
     echo Enter new file name:
     read file_name
+fi
 
-echo '#!/bin/bash' > $file_name
+echo "Trying to create $file_name ..."
+[[ ! $file_name ]] && echo No file name && exit
+[[ -e $file_name ]] && echo The file or directory is exist && exit
+
+[[ -f ~/.templates/sh_header.templ ]] && cat ~/.templates/sh_header.templ > $file_name
+echo continuin:wq
+#[[ -f ~/.templates/sh_header.templ ]] && echo GOOD
+#echo not GOOD
+
+#echo '#!/bin/bash' > $file_name
 chmod u+x $file_name
 nvim $file_name
 
