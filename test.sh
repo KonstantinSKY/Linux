83c44079d35f4b67ea757f36c0132aab2860211e
sky <sky012877@gmail.com>
2021-04-01 16:09:30 -0700

    NO MESSAGE

--- test.sh
+++ test.sh
@@ -1,17 +1,23 @@
-echo $a
+#echo $a
+str1=$(printf '#%.0s' {1..80})
+#echo "$(printf '#\e[79b\n')" > file
+echo $str1 > file
 
-printf '#\e[79b\n'
 format='# %-11s : %-32s %-6s : %-21s#\n'
 format2='# %-11s : %-63s#\n'
-printf "$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY" 
-printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com"
-printf "$format2" "Description" ""
-printf '#\e[79b\n\n'
-
-str1="$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY" 
-printf $str1
+printf "$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY" >> file
+printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com" >> file
+printf "$format2" "Description" "" >> file
+echo $str1 >> file
 
 
-printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com"
-printf "$format2" "Description" ""
-printf '#\e[79b\n\n'
+#printf '#\e[79b\n\n' >> file
+
+
+#str1="$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY" 
+#printf $str1
+
+
+#printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com"
+#printf "$format2" "Description" ""
+#printf '#\e[79b\n\n'
