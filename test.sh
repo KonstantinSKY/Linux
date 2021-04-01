069ed726bcb02f9740d0a6a7f36bed8bf6d4de77
sky <sky012877@gmail.com>
2021-04-01 00:30:42 -0700

    NO MESSAGE

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo $a
-printf '# Script name : %s%40s Date :%s%40s\n' "nadf:e" "MM/DD/YY" '#'
-printf '# Script name : %s%40s%s%20s#\n' "nadcdfddf:e" "Date : MM/DDsfdsf/YY"
-
-printf '# %-11s : %-32s %-7s : %-20s#\n' "Scripe" "nadcdfddf:e"  "Date" "M/DDsfdsf/YY"
-printf '# Scipt name : %s%40s%-7s : %20s#\n' "nadcdfddf:e" "Date" " MM/DDsfdsf/YY"
+#printf '# Scrpt name : %s%40s Date :%s%40s\n' "nadf:e" "MM/DD/YY" '#'
+#printf '# Script name : %s%40s%s%20s#\n' "nadcdfddf:e" "Date : MM/DDsfdsf/YY"
+format='# %-11s : %-32s %-7s : %-20s#\n'
+printf "$format" "Scripe" "nadcdfddf:e"  "Date" "M/DDsfdsf/YY"
+printf '# %-11s : %-32s %-7s : %-20s#\n' "Scripe" "nddf:e"  "Date" "Msfdsf/YY"
 printf '#%.0s' {1..80}
