88c192ed044c87d8c6d9c173716e688bd844d711
sky <sky012877@gmail.com>
2021-04-01 10:19:12 -0700

    NO MESSAGE

--- new.sh
+++ new.sh
@@ -20,15 +20,17 @@ echo "Trying to create $file_name ..."
 #echo not GOOD
 
 #echo '#!/bin/bash' > $file_name
- #printf '# Scrpt name : %s%40s Date :%s%40s\n' "nadf:e" "MM/DD/YY" '#'
-  3 #printf '# Script name : %s%40s%s%20s#\n' "nadcdfddf:e" "Date : MM/DDsfdsf/YY"                     
+#printf '# Scrpt name : %s%40s Date :%s%40s\n' "nadf:e" "MM/DD/YY" '#'
+#printf '# Script name : %s%40s%s%20s#\n' "nadcdfddf:e" "Date : MM/DDsfdsf/YY"                     
 format='# %-11s : %-32s %-6s : %-21s#\n'
 format2='# %-11s : %-63s#\n'
-printf '#\e[79b\n'
-printf "$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY"
-printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com"
-printf "$format2" "Description" ""
-printf '#\e[79b\n'
+str1=$(printf '#%.0s' {1..80})
+str2=$(printf "$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY")
+str3=$(printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com")
+str4=$(printf "$format2" "Description" "")
+#str5=printf '#\e[79b\n' > $file_name
+echo $str1 \
+    $str2 > $file_name
 
 chmod u+x $file_name
 nvim $file_name
--- test.sh
+++ test.sh
@@ -3,7 +3,15 @@ echo $a
 printf '#\e[79b\n'
 format='# %-11s : %-32s %-6s : %-21s#\n'
 format2='# %-11s : %-63s#\n'
-printf "$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY"
+printf "$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY" 
+printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com"
+printf "$format2" "Description" ""
+printf '#\e[79b\n\n'
+
+str1="$format" "Script name" "var_name"  "Date" "M/DDsfdsf/YY" 
+printf $str1
+
+
 printf "$format" "Author" "Stan SKY" "E-mail" "sky012877@gmail.com"
 printf "$format2" "Description" ""
 printf '#\e[79b\n\n'
