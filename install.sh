1e48e90ddc71c8e22799391e31e819a365ef483a
sky <sky012877@gmail.com>
2021-04-08 10:09:17 -0700

    added header

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+################################################################################
+# Script name : install.sh                       Date   : 04/08/2021           #
+# Author      : Stan SKY                         E-mail : sky012877@gmail.com  #
+# Description : Install programm for all Linux distr                           #
+################################################################################
 
-#Installing Linux programm
 
 echo Installing vim-plug...
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
